23d70ff8f89992e4b62b3e3f5ec10d309da54e8b
Rafi <hrakibul60@gmail.com>
2019-07-24 14:38:52 +0600

    Sub-title updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to Health Plus</div>
+                <div class="panel-heading">Welcome to Health Plus!</div>
 
                 <div class="panel-body">
-                    A digitalized health care system in Bangladesh
+                    A digitalized health Service system in Bangladesh
                 </div>
             </div>
         </div>
